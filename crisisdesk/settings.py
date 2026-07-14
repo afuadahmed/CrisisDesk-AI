3a995377550b810b0e2d5378bc9aef33098181dc
Fuad Ahmed <afuadahmed@gmail.com>
2026-07-14 15:50:19 +0600

    Configure PostgreSQL database for persistent storage

--- crisisdesk/settings.py
+++ crisisdesk/settings.py
@@ -3,6 +3,7 @@ Django settings for crisisdesk project.
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 
@@ -89,10 +90,11 @@ WSGI_APPLICATION = "crisisdesk.wsgi.application"
 # Database
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
